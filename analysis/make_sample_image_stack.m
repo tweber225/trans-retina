d2f32a98e1b374e43ec66c867055b82db14cf88c
Tim Weber <tweber225@gmail.com>
2017-10-31 09:41:03 -0400

    edits while in mexico

---
+++ analysis/make_sample_image_stack.m
@@ -0,0 +1,110 @@
+% Quick script to make a stack of sample images
+% Translates, rotates, and varies the whole image intensity
+clear all;
+numFrames = 25;
+baseImg = double(imread('cameraman.tif'));
+rotList = (9 + 1*randn(numFrames,1)); % degrees to randomly translate
+transList = 1*randn(numFrames,2);  % pixels x,y to randomly translate
+intensityList = 1+0.1*randn(numFrames,1); % factor to multiply original image values by
+noiseStd = 256*.01;
+
+% Loop through and transform frames
+maxShift = ceil(max(abs(transList)));
+padBaseImg = padarray(baseImg,maxShift,0);
+unregStack = zeros(size(padBaseImg,1),size(padBaseImg,2),numFrames);
+padBaseImgSize = size(padBaseImg);
+
+for frameIdx = 2:numFrames
+    padRotImg = imrotate(padBaseImg,rotList(frameIdx),'crop','bilinear');
+    frameTForm = affine2d([1 0 0; 0 1 0; transList(frameIdx,1) transList(frameIdx,2) 1]);
+    unregStack(:,:,frameIdx) = intensityList(frameIdx)*imwarp(padRotImg,frameTForm,'OutputView',imref2d(padBaseImgSize));
+end
+unregStack(:,:,1) = padBaseImg;
+rotList(1) = 0;
+transList(1,:) = [0 0];
+intensityList(1) = 1;
+
+%% Find suitable area to crop
+zerosStack = sum((unregStack == 0),3);
+zeroImg = (zerosStack~=0);
+
+% work out from center checking whether we have hit non-zeros
+xLeftIdx = 1;
+xLeft = 1;
+xRightIdx = 1;
+xRight = 1;
+yBottomIdx = 1;
+yBottom = 1;
+yTopIdx = 1;
+yTop = 1;
+while (xLeft || xRight || yBottom || yTop)
+    if xLeft
+        xLeftIdx = xLeftIdx + 1;
+        if (sum(sum(zeroImg((round(padBaseImgSize(1)/2)-yTopIdx):(round(padBaseImgSize(1)/2)+yBottomIdx),(round(padBaseImgSize(2)/2)-xLeftIdx):(round(padBaseImgSize(2)/2)+xRightIdx))))>0)
+            xLeftIdx = xLeftIdx -1;
+            xLeft = 0;
+        end
+    end
+    
+    if xRight
+        xRightIdx = xRightIdx + 1;
+        if (sum(sum(zeroImg((round(padBaseImgSize(1)/2)-yTopIdx):(round(padBaseImgSize(1)/2)+yBottomIdx),(round(padBaseImgSize(2)/2)-xLeftIdx):(round(padBaseImgSize(2)/2)+xRightIdx))))>0)
+            xRightIdx = xRightIdx -1;
+            xRight = 0;
+        end
+    end
+    
+    if yBottom
+        yBottomIdx = yBottomIdx + 1;
+        if (sum(sum(zeroImg((round(padBaseImgSize(1)/2)-yTopIdx):(round(padBaseImgSize(1)/2)+yBottomIdx),(round(padBaseImgSize(2)/2)-xLeftIdx):(round(padBaseImgSize(2)/2)+xRightIdx))))>0)
+            yBottomIdx = yBottomIdx -1;
+            yBottom = 0;
+        end
+    end
+    
+    if yTop
+        yTopIdx = yTopIdx + 1;
+        if (sum(sum(zeroImg((round(padBaseImgSize(1)/2)-yTopIdx):(round(padBaseImgSize(1)/2)+yBottomIdx),(round(padBaseImgSize(2)/2)-xLeftIdx):(round(padBaseImgSize(2)/2)+xRightIdx))))>0)
+            yTopIdx = yTopIdx -1;
+            yTop = 0;
+        end
+        
+    end
+end
+
+% Make the cropped area square and also centered
+[~,minDim] = min([(yTopIdx+yBottomIdx+1),(xLeftIdx+xRightIdx+1)]);
+if (yTopIdx+yBottomIdx+1) == (xLeftIdx+xRightIdx+1)
+    % then nothing is necessary, the cropped frame is will be square
+elseif minDim == 1
+    % Then x>y
+    pixDiff = (xLeftIdx+xRightIdx+1) - (yTopIdx+yBottomIdx+1);
+    xLeftIdx = xLeftIdx-floor(pixDiff/2);
+    xRightIdx = xRightIdx-ceil(pixDiff/2);
+else
+    % Then y>x
+    pixDiff = (yTopIdx+yBottomIdx+1) - (xLeftIdx+xRightIdx+1);
+    yTopIdx = yTopIdx - floor(pixDiff/2);
+    yBottomIdx = yBottomIdx - ceil(pixDiff/2);
+end
+
+% Also try to make even
+if (mod((yTopIdx+yBottomIdx+1),2) ~= 0)
+    yBottomIdx = yBottomIdx -1;
+    xRightIdx = xRightIdx -1;
+end
+
+cropUnregStack = unregStack((round(padBaseImgSize(1)/2)-yTopIdx):(round(padBaseImgSize(1)/2)+yBottomIdx),(round(padBaseImgSize(2)/2)-xLeftIdx):(round(padBaseImgSize(2)/2)+xRightIdx),:);
+
+% Finally add noise
+cropUnregStack = cropUnregStack + noiseStd*randn(size(cropUnregStack));
+
+for frameIdx = 1:numFrames
+    imagesc(cropUnregStack(:,:,frameIdx));drawnow;
+
+end
+
+
+
+
+
--- analysis/register_single_color_fundus.m
+++ analysis/register_single_color_fundus.m
@@ -1,33 +1,38 @@
-function [regStack xShift yShift thetaShift] = register_single_color_fundus(unregStack,latUpSample,rotUpSample)
+function [regStack, xShift, yShift, rotEst] = register_single_color_fundus(unregStack,latUpsample,rotUpsample)
 % ANALYSIS/REGISTER_SINGLE_COLOR_FUNDUS Function to register a series of
 % fundus images from a single color channel. Uses upsampled phase
 % correlations to detect lateral and rotational image shifts. The degree of
 % upsampling in lateral and rotational directions is given by input
-% parameters "latUpSample" and "rotUpSample", respectively. Defaults
+% parameters "latUpsample" and "rotUpsample", respectively. Defaults
 % registration precision before upsampling is 1 pixel (lateral) and
 % 360/[circumference of inscribed circle] degrees (rotational).
 % 
 % Part 2 of standard image processing pipeline
 % Timothy D. Weber
 % Biomicroscopy Lab, BU 2017
+
 tic
 % INITIAL PARAMETERS
-[xPix, yPix, numFrames] = size(unregStack);
+[yPix, xPix, numFrames] = size(unregStack);
 imgDiam = min([xPix yPix]);
 numInitialAngles = 2*round((imgDiam*pi)/2); % Force the number angles to be even (necessary for steps below)
 % FFT2 WHOLE STACK
-FFT2UnregStack = fftshift(fft2(unregStack),1);
-FFT2UnregStack = fftshift(FFT2UnregStack,2); % and fftshift dimensions 1 and 2
+FFT2UnregStack = zeros(yPix,xPix,numFrames);
+for frameIdx = 1:numFrames
+    FFT2UnregStack(:,:,frameIdx) = fftshift(fft2(unregStack(:,:,frameIdx))); % and fftshift dimensions 1 and 2
+end
 
-
-% ROTATIONAL REGISTRATION
+% ROTATIONAL REGISTRATION 
 % The idea here is we have multiple concentric rings of values around the
 % center of the fft magnitude spectrum. We take Fourier transforms along
 % these rings giving multiple realizations of the rotational shift. We can
-% estimate the rotation by a weighted average of these realizations.
+% estimate the rotation by a weighted average of phase correlations from
+% these realizations.
 
 % Make indices of coordinates
-anglesRad = deg2rad(0:(360/numInitialAngles):(360/numInitialAngles)*(numInitialAngles-1));
+startAngle = 360/(2*numInitialAngles);
+anglePitch = 360/numInitialAngles;
+anglesRad = deg2rad(startAngle:anglePitch:360);
 radii = 1:floor(0.5*(imgDiam-1));
 [polCoordsTheta, polCoordsR] = meshgrid(anglesRad,radii);
 [cartNormalCoordsX, cartNormalCoordsY] = meshgrid((-(xPix-1)/2):((xPix-1)/2),(-(yPix-1)/2):((yPix-1)/2));
@@ -38,17 +43,22 @@ warpedFirstFrame = interp2(cartNormalCoordsX, cartNormalCoordsY, abs(FFT2UnregSt
 rotFFTFirstFrame = fft(warpedFirstFrame,[],2);
 
 % Calculate DFT matrix parameters for upsampled phase-correlation
-totalUpSampleFreqs = numInitialAngles*rotUpSample;
+totalUpsampleFreqs = numInitialAngles*rotUpsample;
 
 % Create weighting matrix for rotational realizations
 weightMat = repmat(radii',[1 numInitialAngles]);
+
+% Lateral registration variables
+FFT2FirstFrame = FFT2UnregStack(:,:,1);
+xShift = zeros(numFrames,1);
+yShift = zeros(numFrames,1);
 
 % Compute a handle omega power function ("twiddle" factor), better to
 % compute this way rather than raising a double-precision number to various
 % large powers
 omegaFactor = @(idx,N) exp(-2*pi*1j*idx/N);
 
-
+% Loop through frames
 for frameIdx = 2:numFrames
     % Interpolate along circular tracks around the center of the image, to
     % make warped image
@@ -62,38 +72,87 @@ for frameIdx = 2:numFrames
     % its circumference (correction for unequal interpolated sampling)
     xPowSpec = rotFFTFirstFrame.*conj(rotFFTFrame)./abs(rotFFTFirstFrame.*conj(rotFFTFrame));
     xCorr = ifft(xPowSpec,[],2);
-    [~, rotCoarseEst] = max(sum(xCorr.*weightMat,1)./sum(weightMat,1));
+    [~, rotCoarseEst] = max(real(sum(xCorr.*weightMat,1)./sum(weightMat,1)));
     rotCoarseEst = rotCoarseEst-1; % Minus one because finding a max at the first element would mean no shift
-    
+
     % With the coarse estimate of the rotation, we can formulate a more
     % precise (inverse) DFT matrix just around this estimate on the
     % cross-power spectrum (not the whole frequency space, since we can be
     % sure the max of the cross-correlation will not be anywhere else)
-    freqsToUseFineGrid = floor((rotCoarseEst-2)*rotUpSample):ceil((rotCoarseEst+2)*rotUpSample);
+    freqsToUse = floor((rotCoarseEst-2)*rotUpsample):ceil((rotCoarseEst+2)*rotUpsample);
     phaseIdxLeft = 0:(numInitialAngles/2-1);
-    phaseIdxRight = (totalUpSampleFreqs-(numInitialAngles/2)):(totalUpSampleFreqs-1);
-    phaseFreqIdxMat = freqsToUseFineGrid'*[phaseIdxLeft, phaseIdxRight];
+    phaseIdxRight = (totalUpsampleFreqs-(numInitialAngles/2)):(totalUpsampleFreqs-1);
+    phaseFreqIdxMat = freqsToUse'*[phaseIdxLeft, phaseIdxRight];
     
     % Note the awkard amount of transposes is so that we can set up the DFT
     % matrix in the conventional configuration (i.e. wikipedia's)
-    targetedDFTMat = omegaFactor(phaseFreqIdxMat,totalUpSampleFreqs); % Neglecting normalization factor
+    targetedDFTMat = omegaFactor(phaseFreqIdxMat,totalUpsampleFreqs); % Neglecting normalization factor
     
     % Compute the upsampled DFT in the neighborhood of coarse estimate
     upsampledXCorr = (targetedDFTMat*(xPowSpec)')';
     
     % weight by the circumference of each circular track
-    upsampledWeightMat = repmat(radii',[1 size(freqsToUseFineGrid,2)]);
-    [~, maxIdx] = max(sum(upsampledXCorr.*upsampledWeightMat,1));
-    upsampledRotEst(frameIdx) = 360*freqsToUseFineGrid(maxIdx)/totalUpSampleFreqs;
+    upsampledWeightMat = repmat(radii',[1 size(freqsToUse,2)]);
+    [~, maxIdx] = max(real(sum(upsampledXCorr.*upsampledWeightMat,1)));
+    rotEst(frameIdx) = mod(360*freqsToUse(maxIdx)/totalUpsampleFreqs+180,360)-180;
+    
+    % Rotate frame by detected ammount
+    rotFrame = imrotate(unregStack(:,:,frameIdx),-rotEst(frameIdx),'crop','bilinear');
+    
+    
+    % LATERAL REGISTRATION
+    % The same idea as before--determine a rough estimate of the lateral
+    % displacement from a phase correlation, then determine sub-pixel
+    % registration from upsampled DFT just around that location
+    FFT2RotFrame = fftshift(fft2(rotFrame));
+    
+    % calculate cross power spectrum and cross correlation
+    xPowSpec = ifftshift(FFT2FirstFrame.*conj(FFT2RotFrame)./abs(FFT2FirstFrame.*conj(FFT2RotFrame)));
+    xCorr = ifft2(xPowSpec);
+    
+    % Get the max in both x and y
+    [~, transCoarseEstX] = max(max(real(xCorr),[],1));
+    [~, transCoarseEstY] = max(max(real(xCorr),[],2));
+    transCoarseEstX = (transCoarseEstX-1);
+    transCoarseEstY = (transCoarseEstY-1);
+    disp([transCoarseEstX transCoarseEstY])
+
+    
+    totalUpsampleFreqsX = xPix*latUpsample;
+    totalUpsampleFreqsY = yPix*latUpsample;
+    % The targeted DFT in this case needs to be 2D, so we will need to
+    % multiply 3 matrices: 1 "left" matrix, responsible for fourier
+    % transforming along Y dimensions, 1 image matrix, and 1 "right"
+    % matrix, responsible for Fourier transforming along X dimension.
+    freqsToUseX = ((transCoarseEstX-3)*latUpsample):((transCoarseEstX+3)*latUpsample);
+    freqsToUseY = ((transCoarseEstY-3)*latUpsample):((transCoarseEstY+3)*latUpsample);
+    phaseLeft = 0:(yPix/2-1);
+    phaseRight = (totalUpsampleFreqsY-(yPix/2)):(totalUpsampleFreqsY-1);
+    phaseBottom = (totalUpsampleFreqsX-(xPix/2)):(totalUpsampleFreqsX-1);
+    phaseTop = 0:(xPix/2-1);
+    phaseFreqIdxMatY = freqsToUseY'*[phaseLeft phaseRight];
+    phaseFreqIdxMatX = [phaseTop';phaseBottom']*freqsToUseX;
+    
+    % Multiply to get DFT-ed cross-correlation
+    upsampledXCorr = (omegaFactor(phaseFreqIdxMatY,totalUpsampleFreqsY)*(xPowSpec))*omegaFactor(phaseFreqIdxMatX,totalUpsampleFreqsX);
+    
+    imagesc(real(upsampledXCorr));drawnow;pause(.1)
+
+    % Find new max
+    [~, maxIdxX] = max(max(real(upsampledXCorr),[],1)); 
+    [~, maxIdxY] = max(max(real(upsampledXCorr),[],2));
+    
+    xShift(frameIdx) = xPix*freqsToUseX(maxIdxX)/totalUpsampleFreqsX;
+    yShift(frameIdx) = yPix*freqsToUseY(maxIdxY)/totalUpsampleFreqsY;
 end
-
-
 
 toc
 
 
+regStack = 0;
 
 
 
 
-% LATERAL REGISTRATION
+
+

aa8a7255f5206110d64a41a7b2cff01bde1e071b
Tim Weber <tweber225@gmail.com>
2019-04-01 11:26:31 -0400

    minor name change

--- analysis/manual_segment_track.m
+++ analysis/manual_segment_track.m
@@ -2,21 +2,19 @@ function pointList = manual_segment_track(img)
 % Function takes in an image and prompts user to manually track the center
 % of a vessel. Outputs a Nx2 list (x and y coordinates) of user-selected
 % points
-scaleFactor = 5;
-blurFactor = 10;
+scaleFactor = 8;
+blurFactor = 12;
 
-% First normalize the image contrast
-imgMax = max(img(:));
-imgMin = min(img(:));
+% Normalize the image contrast
+img = single(img);
+imgMax = max(img(:)); imgMin = min(img(:));
 imgRange = imgMax-imgMin;
 normImg = (img-imgMin)/imgRange;
 
 % Zoom into area of interest first
 imshow(normImg)
-[x,y] = getpts;
-x = round(x);
-y = round(y);
-close
+[x,y] = getpts; close
+x = round(x); y = round(y);
 
 % Ask user how many pixels width to display
 prompt = 'How many pixels width for the zoomed region? ';
@@ -28,7 +26,7 @@ yMin = y-zoomWidth/2+1; yMax = y+zoomWidth/2;
 cropImg = normImg(yMin:yMax,xMin:xMax);
 
 % Zoom image and flatten contrast for best visibilty
-zoomedImg = imresize(cropImg,scaleFactor);
+zoomedImg = imresize(cropImg,scaleFactor,'nearest');
 blurredImg = imgaussfilt(zoomedImg,scaleFactor*blurFactor);
 flattenImg = zoomedImg./blurredImg;
 imgMax = max(flattenImg(:));
@@ -41,4 +39,5 @@ imshow(flattenNormImg)
 [xi,yi] = getpts;
 close
 
+%
 

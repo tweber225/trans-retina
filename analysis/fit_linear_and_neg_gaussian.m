126a9e4d9bf6c49f84c20d4211b7febdd7d454bc
Tim Weber <tweber225@gmail.com>
2019-04-02 19:00:01 -0400

    vessel cross section model fitting attempt 1

--- analysis/fit_linear_and_neg_gaussian.m
+++ analysis/fit_linear_and_neg_gaussian.m
@@ -1,11 +1,28 @@
 function [fitresult, gof] = fit_linear_and_neg_gaussian(x, y)
 %FIT_LINEAR_AND_NEG_GAUSSIAN(X,Y)
 % Creates a fit of the x,y data to a linear function (mx+b) and a Gaussian.
-%
-% Linear function's intercept is assumed to be positive.
-% Gaussian function's center is assumed to be in the domain of the data
-% Gaussian's amplitude is assumed to be negative
 
+% Find number of points in cross section
+numPoints = numel(x);
+
+% Define constraints
+aMin = 0; % intercept cannot be negative as we have positive only signals
+aMax = 2^16; % max level on camera
+bMin = -Inf;
+bMax = Inf;
+cMin = -Inf;
+cMax = -1;
+dMin = 2; % tracking vessels of diameter > 2 pixels
+dMax = numPoints/2; % can't be larger than half the cross section
+eMin = -numPoints/4; % center must be in the center half of the cross section
+eMax = numPoints/4; % center must be in the center half of the cross section
+
+% Define starting guess
+aGuess = mean(y);
+bGuess = 0;
+cGuess = -mean(y)/100;
+dGuess = numPoints/4;
+eGuess = 0;
 
 %% Fit: 'untitled fit 1'.
 [xData, yData] = prepareCurveData( x, y );
@@ -14,20 +31,20 @@ function [fitresult, gof] = fit_linear_and_neg_gaussian(x, y)
 ft = fittype( 'a + b*x + c*exp(-4*0.6931471806*((x-e)./d)^2)', 'independent', 'x', 'dependent', 'y' );
 opts = fitoptions( 'Method', 'NonlinearLeastSquares' );
 opts.Display = 'Off';
-opts.Lower = [0 -Inf -Inf 0 min(x)];
-opts.StartPoint = [1 1 -.1 1 max(x)/2];
-opts.Upper = [Inf Inf 0 Inf max(x)];
+opts.Lower = [aMin bMin cMin dMin eMin];
+opts.StartPoint = [aGuess bGuess cGuess dGuess eGuess];
+opts.Upper = [aMax bMax cMax dMax eMax];
 
 % Fit model to data.
 [fitresult, gof] = fit( xData, yData, ft, opts );
 
-% Plot fit with data.
-%figure( 'Name', 'untitled fit 1' );
-h = plot( fitresult, xData, yData );
-legend( h, 'y vs. x', 'untitled fit 1', 'Location', 'NorthEast' );
-% Label axes
-xlabel x
-ylabel y
-grid on
+% % Plot fit with data.
+% %figure( 'Name', 'untitled fit 1' );
+% h = plot( fitresult, xData, yData );
+% legend( h, 'y vs. x', 'untitled fit 1', 'Location', 'NorthEast' );
+% % Label axes
+% xlabel x
+% ylabel y
+% grid on
 
 
--- analysis/segment_vessel_network.m
+++ analysis/segment_vessel_network.m
@@ -1,13 +1,58 @@
-function segment_vessel_network(segments)
+function segment_vessel_network(segments,fundusImg)
 
 % Function that takes in vessel segment points in segments structure
 
-
+numSegments = numel(segments);
 
 % Spline interpolate evenly-spaced points along vessel segment paths
 pointSpacing = 1;
 interpSegments = spline_interp_paths(segments,pointSpacing);
 
 % Fit the segment cross sections to initial model
-fitSegments = fit_segments_to_model(interpSegments,'linear and negative exponential');
+crossSectionLength = 32;
+fitSegments = fit_segments_to_model(interpSegments,fundusImg,crossSectionLength,'linear and negative exponential');
+
+% Draw vessel bounds lines
+figure;
+imshow(norm_contrast(fundusImg)); 
+hold on;
+for segmentIdx = 1:numSegments
+    % Draw lower line
+    x = interpSegments(segmentIdx).xPoints - cos(interpSegments(segmentIdx).angles-pi/2).*fitSegments(segmentIdx).FWHM/2;
+    y = interpSegments(segmentIdx).yPoints - sin(interpSegments(segmentIdx).angles-pi/2).*fitSegments(segmentIdx).FWHM/2;
+    line(x,y);
+    
+    % Draw upper line
+    x = interpSegments(segmentIdx).xPoints + cos(interpSegments(segmentIdx).angles-pi/2).*fitSegments(segmentIdx).FWHM/2;
+    y = interpSegments(segmentIdx).yPoints + sin(interpSegments(segmentIdx).angles-pi/2).*fitSegments(segmentIdx).FWHM/2;
+    line(x,y);
+    
+end
+
+% Try smoothing fit parameters
+smoothSpan = 20;
+figure;
+imshow(norm_contrast(fundusImg)); 
+hold on;
+for segmentIdx = 1:numSegments
+    fitSegments(segmentIdx).smoothFWHM = smooth(fitSegments(segmentIdx).FWHM,smoothSpan,'rlowess');
+    
+    % Draw lower line
+    x = interpSegments(segmentIdx).xPoints - cos(interpSegments(segmentIdx).angles-pi/2).*fitSegments(segmentIdx).smoothFWHM/2;
+    y = interpSegments(segmentIdx).yPoints - sin(interpSegments(segmentIdx).angles-pi/2).*fitSegments(segmentIdx).smoothFWHM/2;
+    line(x,y);
+    
+    % Draw upper line
+    x = interpSegments(segmentIdx).xPoints + cos(interpSegments(segmentIdx).angles-pi/2).*fitSegments(segmentIdx).smoothFWHM/2;
+    y = interpSegments(segmentIdx).yPoints + sin(interpSegments(segmentIdx).angles-pi/2).*fitSegments(segmentIdx).smoothFWHM/2;
+    line(x,y);
+    
+end
+
+% Draw polygon ROI
+
+polyROI = drawpolygon('Position',[x,y]);
+
+
+spy
 
